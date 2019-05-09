00fdf9239d792757a6cb5c3bfe150d79d6790ad4
Шарова Елена <e.sharova@cian.ru>
2019-05-09 19:25:31 +0300

    feat(*): real request

--- src/api-decision.ts
+++ src/api-decision.ts
@@ -5,20 +5,25 @@ export interface IDecision {
     offers?: IOffer[];
 }
 
+// export function fetchDecisionStatus(): Promise<IDecision> {
+//     return Promise.resolve({
+//         status: 'SUCCESS',
+//         offers: [
+//             {
+//                 rate: 10.05,
+//                 monthlyPayment: 52000,
+//                 term: 20
+//             },
+//             {
+//                 rate: 11.05,
+//                 monthlyPayment: 62000,
+//                 term: 20
+//             }
+//         ]
+//     });
+// }
+
 export function fetchDecisionStatus(): Promise<IDecision> {
-    return Promise.resolve({
-        status: 'SUCCESS',
-        offers: [
-            {
-                rate: 10.05,
-                monthlyPayment: 52000,
-                term: 20
-            },
-            {
-                rate: 11.05,
-                monthlyPayment: 62000,
-                term: 20
-            }
-        ]
-    });
+    return fetch('http://localhost:8080/offers', { method: 'GET'})
+        .then(response => response.json());
 }
